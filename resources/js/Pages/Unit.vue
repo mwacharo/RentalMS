a659b644089d485cd267c46796f5225e9a1b8dbf
mwacharo <mwacharommwanyolo@gmail.com>
2024-04-27 07:11:02 +0300

    getBookingsByProperty

--- resources/js/Pages/Unit.vue
+++ resources/js/Pages/Unit.vue
@@ -336,7 +336,7 @@ export default {
                 });
         },
         fetchProperties() {
-            const API_URL = "property";
+            const API_URL = "properties";
             axios
                 .get(API_URL)
                 .then((response) => {
